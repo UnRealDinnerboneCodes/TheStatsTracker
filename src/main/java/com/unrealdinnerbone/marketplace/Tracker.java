31cebdaf44445d4cef862c1dcbe97fa02a70f498
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2024-01-03 10:34:04 -0600

    general_time

--- src/main/java/com/unrealdinnerbone/marketplace/Tracker.java
+++ src/main/java/com/unrealdinnerbone/marketplace/Tracker.java
@@ -1,5 +1,8 @@
 package com.unrealdinnerbone.marketplace;
 
+import com.unrealdinnerbone.config.api.ConfigCreator;
+import com.unrealdinnerbone.config.api.exception.ConfigException;
+import com.unrealdinnerbone.config.config.ConfigValue;
 import com.unrealdinnerbone.config.impl.provider.EnvProvider;
 import com.unrealdinnerbone.postgresslib.PostgresConfig;
 import com.unrealdinnerbone.postgresslib.PostgressHandler;
@@ -19,11 +22,21 @@ public class Tracker
     public static void main(String[] args) throws SQLException {
         LOGGER.info("Loading Me!");
         EnvProvider<PostgresConfig> envProvider = new EnvProvider<>();
-        PostgresConfig postgresConfig = envProvider.loadConfig(PostgresConfig::new);
+        PostgresConfig postgresConfig = envProvider.loadConfig("postgres", PostgresConfig::new);
+        Config config = envProvider.loadConfig("general", Config::new);
+        try {
+            envProvider.read();
+        } catch (ConfigException e) {
+            LOGGER.error("Error while loading config", e);
+            return;
+        }
         LOGGER.info("Host: {} Port: {} Database: {} Username: {} Password: {}", postgresConfig.getHost().get(), postgresConfig.getPort().get(), postgresConfig.getDb().get(), postgresConfig.getUsername().get(), postgresConfig.getPassword().get());
+        LOGGER.info("Running tracker every {} hours", config.getTime().get());
         PostgressHandler postgressHandler = new PostgressHandler(postgresConfig);
-        register(postgressHandler, TimeUnit.HOURS, 12, new CurseforgeTracker());
-        register(postgressHandler, TimeUnit.HOURS, 1, new CurseforgeStoreTracker());
+        register(postgressHandler, TimeUnit.HOURS, config.getTime().get(), new CurseforgeTracker());
+        if(config.getEnableStore().get()) {
+            register(postgressHandler, TimeUnit.HOURS, config.getTime().get(), new CurseforgeStoreTracker());
+        }
 
     }
 
@@ -38,6 +51,23 @@ public class Tracker
                 }
             }
         });
+    }
+
+    public static class Config {
+
+        private final ConfigValue<Integer> time;
+        private final ConfigValue<Boolean> enableStore;
+        public Config(ConfigCreator creator) {
+            this.time = creator.createInteger("time", 12);
+            this.enableStore = creator.createBoolean("enableStore", false);
+        }
+
+        public ConfigValue<Boolean> getEnableStore() {
+            return enableStore;
+        }
+        public ConfigValue<Integer> getTime() {
+            return time;
+        }
     }
 
 
